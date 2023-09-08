ecfef4754091336ef4ef47f56232acc0b3b6ee0f
jedidiah <jedidahfavour@gmail.com>
2023-09-08 14:44:29 +0300

    scrapes data on face masks from jumia.co.ke

--- scrappy.py
+++ scrappy.py
@@ -11,13 +11,29 @@ chrome_service = webdriver.chrome.service.Service(executable_path=chromedriver_p
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 products = []
-ratings = []
+brands = []
 prices = []
+categories = []
 
 url = 'https://www.jumia.co.ke/catalog/?q=face+masks'
 driver.get(url)
 
 data = driver.page_source
 soup = BeautifulSoup(data, features="html.parser")
+articles = soup.findAll('article', attrs={'class':'prd _fb col c-prd'})
 
-# for a in soup.findAll('a', href=True, attr ={}):
+for article in articles:
+    name = article.find('a', class_= 'core')['data-name']
+    brand = article.find('a', class_= 'core')['data-brand']
+    price = article.find('a', class_= 'core')['data-price']
+    category = article.find('a', class_= 'core')['data-category']
+
+    products.append(name)
+    brands.append(brand)
+    prices.append(price)
+    categories.append(category)
+
+df = pd.DataFrame({'Face Mask': products, 'Brand': brands, 'Price': prices, 'Category': categories})
+df.to_csv('facemasks.csv', index=False, encoding='utf-8')
+myFile = pd.read_csv('facemasks.csv')
+print(myFile)
